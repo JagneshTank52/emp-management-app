a15866ad25db998d3148e69d93a4687d771136b2
jagneshtank <tatva.pctr52@outlook.com>
2025-07-31 17:04:15 +0530

    add menu in

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -7,9 +7,18 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthService } from '../core/services/auth.service';
 import { authInterceptor } from '../core/interceptor/auth.interceptor';
 import { authGuard } from '../core/guards/auth-guard';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(
@@ -17,7 +26,7 @@ export const appConfig: ApplicationConfig = {
         authInterceptor
       ])
     ),
-    provideRouter(routes), 
+    provideRouter(routes),
     provideClientHydration(withEventReplay()),
     AuthService,
   ]
--- src/app/shared/main-navbar/main-navbar.ts
+++ src/app/shared/main-navbar/main-navbar.ts
@@ -1,21 +1,72 @@
-import { Component } from '@angular/core';
-import { ReusableBtn } from '../reusable-btn/reusable-btn';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ReusableNavBtn } from '../reusable-nav-btn/reusable-nav-btn';
 import { AuthService } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { Menu, MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import { PopoverModule } from 'primeng/popover';
 
 @Component({
   selector: 'app-main-navbar',
-  imports: [ReusableNavBtn],
+  imports: [ReusableNavBtn, AvatarModule, CommonModule,
+    MenuModule, PopoverModule,
+    ButtonModule,
+  ],
   templateUrl: './main-navbar.html',
   styleUrl: './main-navbar.css'
 })
-export class MainNavbar {
+
+export class MainNavbar implements OnInit {
+  menuItems: MenuItem[] = [];
+  selectedMember!: MenuItem;
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
+
+  @ViewChild('menu') menu!: Menu;
+  
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'My Profile',
+        icon: 'pi pi-user',
+        command: () => this.goToProfile()
+      },
+      {
+        label: 'Change Password',
+        icon: 'pi pi-lock',
+        command: () => this.changePassword()
+      },
+      {
+        label: 'Logout',
+        icon: 'pi pi-sign-out',
+        command: () => this.logout()
+      }
+    ];
+  }
+  toggleMenu(event: Event) {
+    this.menu.toggle(event);
+  }
+
+  selectMember(member: MenuItem) {
+    this.selectedMember = member
+  }
+
+  goToProfile() {
+    console.log("Navigating to profile...");
+    // this.router.navigate(['/profile']);
+  }
+
+  changePassword() {
+    console.log("Navigating to change-password...");
+    // this.router.navigate(['/change-password']);
+  }
 
   logout(): void {
     debugger;
@@ -28,7 +79,7 @@ export class MainNavbar {
       },
       error: (err) => {
         console.error('Unexpected logout error:', err);
-        this.router.navigate(['/auth/login']); 
+        this.router.navigate(['/auth/login']);
       }
     });
   }
